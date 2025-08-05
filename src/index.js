a1edaf4936d714183b6b5417b36e52beff3c5e19
aditya-978 <adityajha978@gmail.com>
2025-08-05 21:47:11 +0530

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -22,14 +22,14 @@ app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
 });
 
-const supabase = require('./utils/supabaseClient.js')
+// const supabase = require('./utils/supabaseClient.js')
 
-async function testSupabase() {
-  const { data, error } = await supabase.from("users").select("*").limit(1);
-  if (error) {
-    console.error("Supabase error:", error.message);
-  } else {
-    console.log("✅ Supabase connected. Sample user data:", data);
-  }
-}
-testSupabase();
+// async function testSupabase() {
+//   const { data, error } = await supabase.from("users").select("*").limit(1);
+//   if (error) {
+//     console.error("Supabase error:", error.message);
+//   } else {
+//     console.log("✅ Supabase connected. Sample user data:", data);
+//   }
+// }
+// testSupabase();
